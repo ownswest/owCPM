6b04af01ae97615ab4cb4e0c82a68734f486e15e
ownswest <105857608+ownswest@users.noreply.github.com>
2022-06-01 11:45:37 +0900

    Update CarParkingMultiplayer_Beta.lua

--- CarParkingMultiplayer_Beta.lua
+++ CarParkingMultiplayer_Beta.lua
@@ -1,1 +1,28 @@
-gg.toast(test)
+--BSD 2-Clause License
+--Copyright (c) 2022, ownswest
+--All rights reserved.
+
+function nowPr(msg)
+  now = os.date("%H:%M:%S ")
+  print(now..msg)
+end
+
+function clrRs()
+  gg.clearResults()
+  nowPr("Result Cleared")
+end
+
+clrRs()
+gg.setVisible(false)
+
+sta = gg.alert("All features offered in the beta can be ben-processed for multiple reasons, so you should refrain from using it as much as possible.\This script is offered free of charge and without warranty.\nUse at your own risk!","Start","Hide")
+if sta==1 then
+  gg.setVisible(true)
+end
+if sta==2 then
+  gg.toast("Press The Icon To Launch")
+end
+
+function run()
+  
+end
