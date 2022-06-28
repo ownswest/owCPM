7a7a6e113b3adbe242520ede3b3c1df74b4bc37a
ownswest <105857608+ownswest@users.noreply.github.com>
2022-06-28 19:07:41 +0900

    Update CarParkingMultiplayer_Main.lua

--- CarParkingMultiplayer_Main.lua
+++ CarParkingMultiplayer_Main.lua
@@ -28,7 +28,7 @@ else
   end
 end
 
-sta = gg.alert("0.1.8\nLast Updated 2022.06.25.15.25.46 (GMT+9)\n\nThe menu with the '⚠' mark will damage your account,\nand the menu with the '❌' mark has been detected!\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!","Start","Hide")
+sta = gg.alert("0.1.9\nLast Updated 2022.06.28.19.00.55 (GMT+9)\n\nThe menu with the '⚠' mark will damage your account,\nand the menu with the '❌' mark has been detected!\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!","Start","Hide")
 if sta==1 then
   gg.setVisible(true)
 end
@@ -114,18 +114,23 @@ function run()
     nowPr("Long Name Selected")
     gg.setVisible(false)
     gg.setRanges(gg.REGION_ANONYMOUS)
-    gg.alert("1")
-    gg.sleep(8000)
-    gg.searchNumber("12;1", gg.TYPE_DWORD)
-    gg.alert("123")
-    gg.sleep(8000)
-    gg.refineNumber("12;3", gg.TYPE_DWORD)
+    gg.alert("If you want to increase the success rate?\nDo not close the 'ENTER YOUR NAME:' settings window!")
+    gg.alert("Set the name to 'owns' in 10 seconds")
+    gg.sleep(10000)
+    gg.searchNumber("12;4", gg.TYPE_DWORD)
     gg.sleep(100)
-    gg.refineNumber("12;3", gg.TYPE_DWORD)
-    gg.sleep(2000)
+    gg.refineNumber("12;4", gg.TYPE_DWORD)
+    gg.alert("Set the name to 'ownswest' in 10 seconds")
+    gg.sleep(10000)
+    gg.refineNumber("12;8", gg.TYPE_DWORD)
+    gg.sleep(100)
+    gg.refineNumber("12;8", gg.TYPE_DWORD)
+    gg.sleep(100)
     gg.refineNumber("12", gg.TYPE_DWORD)
-    gg.getResults(100)
-    gg.editAll("500", gg.TYPE_DWORD)
+    gg.sleep(100)
+    gg.refineNumber("12", gg.TYPE_DWORD)
+    gg.getResults(2)
+    gg.editAll("0", gg.TYPE_DWORD)
     nowPr("Long Name Is Possible")
   end
   
