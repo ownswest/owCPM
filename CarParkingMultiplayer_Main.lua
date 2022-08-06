f6d60379f6f6dae1daa5414f53077da6f97a2318
ownswest <105857608+ownswest@users.noreply.github.com>
2022-08-06 17:06:47 +0900

    Update CarParkingMultiplayer_Main.lua

--- CarParkingMultiplayer_Main.lua
+++ CarParkingMultiplayer_Main.lua
@@ -39,7 +39,7 @@ else
   end
 end
 
-sta = gg.alert("0.3.0\nLast Updated 2022.08.06.16.58.59 (GMT+9)\n\nThe menu with the '⚠' mark will damage your account,\nand the menu with the '❌' mark has been detected!\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!","Start","Hide")
+sta = gg.alert("0.3.0\nLast Updated 2022.08.06.17.06.37 (GMT+9)\n\nThe menu with the '⚠' mark will damage your account,\nand the menu with the '❌' mark has been detected!\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!","Start","Hide")
 if sta==1 then
   gg.setVisible(true)
 end
@@ -68,9 +68,9 @@ function run()
     nowPr("Scanning Engine Values")
     egn=gg.choice({
         
-        '⛽   1695hp(7000)\n    2254nm(3500)',
-        '⛽   1695.999hp(1000)\n    2254.999nm(1010)',
-        '⛽   99.999hp(8000)\n    2299.999nm(7900)',
+        '⛽  1695hp(7000)\n     2254nm(3500)',
+        '⛽  1695.999hp(1000)\n     2254.999nm(1010)',
+        '⛽  99.999hp(8000)\n     2299.999nm(7900)',
         '⛽   Fast Gearbox(1E-10S)'
         
 },nil,' 『Engine』 ')
