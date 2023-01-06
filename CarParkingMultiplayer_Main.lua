cb999f20728b9075b518ee1f95282e9d74630808
ownswest <105857608+ownswest@users.noreply.github.com>
2023-01-06 17:16:00 +0900

    Update CarParkingMultiplayer_Main.lua

--- CarParkingMultiplayer_Main.lua
+++ CarParkingMultiplayer_Main.lua
@@ -133,7 +133,7 @@ else
   os.exit()
 end
 
-sta = gg.alert("❤Script V.0.5.2 2023/01/04 20:15 (GMT+9)❤\nFor 4.8.9.1.9 Carparkingmultiplayer\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!\n\nBSD 2-Clause License\nCopyright (c) 2022, ownswest\nAll rights reserved.","🌞Start🌞","☀Hide☀","❌Exit❌")
+sta = gg.alert("❤Script V.0.5.2 2023/01/04 20:15 (GMT+9)❤\nFor 4.8.9.1.13 Carparkingmultiplayer\n\nThis script is offered free of charge and without warranty.\nUse at your own risk!\n\nBSD 2-Clause License\nCopyright (c) 2022, ownswest\nAll rights reserved.","🌞Start🌞","☀Hide☀","❌Exit❌")
 
 if sta==1 then
   gg.setVisible(true)
@@ -160,7 +160,7 @@ function run()
       '2   😁Long Name😁',
       '3   🚗Get Vehicle🚗',
       '4   🎟Get Money&Coin🎟(x64 only)',
-      '5   ☢Original Server For Blacklisted Engines☢(x64 only)',
+      '5   ☢Original Server For Blacklisted Engines☢(~4.8.8.9)',
       '6   ☀Chrome☀',
       '7   🌟Body Kit🌟',
       '8   🚀Ufo🚀',
