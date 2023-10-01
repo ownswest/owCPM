a5cb947bd13baa9b31117d0efda3aee008ee1071
ownswest <105857608+ownswest@users.noreply.github.com>
2023-10-01 15:28:29 +0900

    Update qq.lua

--- qq.lua
+++ qq.lua
@@ -40,17 +40,17 @@ else
 end
 
 uye = "2023"
-umo = "09"
-uda = "18"
-uho = "00"
-umi = "14"
+umo = "10"
+uda = "01"
+uho = "15"
+umi = "25"
 nye = os.date('%Y')
 nmo = os.date('%m')
 nda = os.date('%d')
 nho = os.date('%H')
 nmi = os.date('%M')
 
-sta = gg.alert("Script Ver 1.9.1 : "..uye.."/"..umo.."/"..uda.." "..uho..":"..umi.." (UTC+9)\nNow : "..nye.."/"..nmo.."/"..nda.." "..nho..":"..nmi.."\nLocale : "..gg.getLocale().."\n\nBSD 2-Clause License\nCopyright (c) 2023, ownswest\nAll rights reserved.","🌞Start🌞","☀Hide☀","🔗Copy Link🔗")
+sta = gg.alert("Script Ver 1.9.3 : "..uye.."/"..umo.."/"..uda.." "..uho..":"..umi.." (UTC+9)\nNow : "..nye.."/"..nmo.."/"..nda.." "..nho..":"..nmi.."\nLocale : "..gg.getLocale().."\n\nBSD 2-Clause License\nCopyright (c) 2023, ownswest\nAll rights reserved.","🌞Start🌞","☀Hide☀","🔗Copy Link🔗")
 
 if sta==1 then
   gg.setVisible(true)
@@ -247,9 +247,9 @@ function run()
             '1989 Mazda MX-5/Miata NA (500)',
             '2022 Porsche 911 GT3 (5000)',
             '2018 Ford F-150 Raptor P552 (1000)',
-            '401 (1000)',
-            '402 (1000)',
-            '403 (1000)'
+            '2017 Mercedes Citaro G1 (1000)',
+            '2022 Dodge Ram TRX 1500 (1000)',
+            '2022 BMW M3 Touring (1000)'
           
           },nil,' 『Coin』 ')
       
